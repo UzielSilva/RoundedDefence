885c1c814af02395f2796ea487758f937d3efbc7
UzielSilva <uziel.silva.espino@gmail.com>
2014-03-30 00:30:05 -0600

    Setting project in x86, adding DrawingArea example.

--- RoundedDefence.View/Program.cs
+++ RoundedDefence.View/Program.cs
@@ -1,16 +1,45 @@
 ﻿using System;
 using Gtk;
+using Pango;
 
-namespace RoundedDefence.View
+class LayoutSample : DrawingArea
 {
-	class MainClass
+	Pango.Layout layout;
+
+	static void Main ()
 	{
-		public static void Main (string[] args)
-		{
-			Application.Init ();
-			MainWindow win = new MainWindow ();
-			win.Show ();
-			Application.Run ();
-		}
+		Application.Init ();
+		new LayoutSample ();
+		Application.Run ();
+	}
+
+	LayoutSample ()
+	{
+		Window win = new Window ("Layout sample");
+		win.SetDefaultSize (400, 300);
+		win.DeleteEvent += OnWinDelete;
+		this.Realized += OnRealized;
+		this.ExposeEvent += OnExposed;
+
+		win.Add (this);
+		win.ShowAll ();
+	}
+
+	void OnExposed (object o, ExposeEventArgs args)
+	{
+		this.GdkWindow.DrawLayout (this.Style.TextGC (StateType.Normal), 100, 150, layout);
+	}
+
+	void OnRealized (object o, EventArgs args)
+	{
+		layout = new Pango.Layout (this.PangoContext);
+		layout.Wrap = Pango.WrapMode.Word;
+		layout.FontDescription = FontDescription.FromString ("Tahoma 16");
+		layout.SetMarkup ("Hello Pango.Layout");
+	}
+
+	void OnWinDelete (object o, DeleteEventArgs args)
+	{
+		Application.Quit ();
 	}
 }
