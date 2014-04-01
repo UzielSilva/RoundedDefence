692761610bce29bdbc1962c60858921a4570cade
UzielSilva <uziel.silva.espino@gmail.com>
2014-04-01 12:25:53 -0600

    Adding Whale and Kraken.

---
+++ RoundedDefence.Components/Fishes/Actives/Whale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace RoundedDefence.Components.Fishes.Actives
+{
+    class Whale : ActiveFish
+
+    {
+        public static Int32 radius = 10;
+        public static Int32 requiredFood = 200;
+        public static Double timeToAction = 2.5;
+        public static Int32 damage = 1000;
+        public Whale()
+            : base(requiredFood, timeToAction, damage)
+        {
+        }
+        public override Boolean IsInArea(Point p)
+        {
+            double x = p.X*Math.Cos(p.Y);
+            double y = p.X*Math.Sin(p.Y);
+            double px = this.Position.X*Math.Cos(this.Position.Y);
+            double py = this.Position.X*Math.Sin(this.Position.Y);
+            if (Math.Pow(x - px, 2) + Math.Pow(y - py, 2) <= Math.Pow(radius,2))
+                return true;
+            return false;
+        }
+    }
+}
--- RoundedDefence.Components/Fishes/Passives/Jellyfish.cs
+++ RoundedDefence.Components/Fishes/Passives/Jellyfish.cs
@@ -5,7 +5,7 @@ namespace RoundedDefence.Components.Fishes.Passives
 {
     class Jellyfish : PassiveFish
     {
-        public static Int32[] Radius = { 4, 5, 6, 7 };
+        public static Int32[] radius = { 4, 5, 6, 7 };
         public static Int32[] requiredFood = { 5, 10, 13, 15 };
         public static Double[] timeToAction = { 1.5, 1.2, 1, 0.8 };
         public static Int32[] damage = { 3, 5, 7, 10 };
@@ -20,8 +20,8 @@ namespace RoundedDefence.Components.Fishes.Passives
             double y = p.X*Math.Sin(p.Y);
             double px = this.Position.X*Math.Cos(this.Position.Y);
             double py = this.Position.X*Math.Sin(this.Position.Y);
-            Int32 radius = Radius[Level];
-            if (Math.Pow(x - px, 2) + Math.Pow(y - py, 2) <= Math.Pow(radius,2))
+            Int32 Radius = radius[Level];
+            if (Math.Pow(x - px, 2) + Math.Pow(y - py, 2) <= Math.Pow(Radius,2))
                 return true;
             return false;
         }
