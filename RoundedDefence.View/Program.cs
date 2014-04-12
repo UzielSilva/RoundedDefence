b7c6027e931508c870b2ae6055cd7b6d7381b8d6
german77 <juangerman-13@hotmail.com>
2014-04-12 11:57:20 -0500

    build test

--- RoundedDefence.View/Program.cs
+++ RoundedDefence.View/Program.cs
@@ -22,17 +22,21 @@ class LayoutSample : Window
 
 	void OnExpose(object sender, ExposeEventArgs args)
 	{
+		int              w, h;
+		Surface *image;
 		DrawingArea area = (DrawingArea) sender;
 		Cairo.Context cr =  Gdk.CairoHelper.Create(area.GdkWindow);
 		//Cairo.Surface image= Cairo.ImageSurface("Resources/mar3.png");
+
 		int width = Allocation.Width;
 		int height= Allocation.Height;
 
 		cr.Rectangle (new Rectangle (0,0,width,height));
-		cr.StrokePreserve();
-
 		cr.SetSourceRGB(0.3, 0.4, 0.6);
-		cr.Fill();
+
+		cr.SetSourceSurface (image, w, h);
+		cr.Paint ();
+
 
 		((IDisposable) cr.Target).Dispose();                                      
 		((IDisposable) cr).Dispose();
