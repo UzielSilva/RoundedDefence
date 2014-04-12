eb4f99d76fdcacfda5e0f4945ada240bb6022c70
german77 <juangerman-13@hotmail.com>
2014-04-12 12:06:44 -0500

    Changing to unity

---
+++ RoundedDefence/Assets/Code/Background.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+    float transparency=0f;
+	bool transDirection=false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position += Vector3.down * Time.deltaTime * 0.1f;
+		transform.Rotate(Vector3.forward, Time.deltaTime); 
+		if (transDirection) {
+			transparency-=.005f;
+			if(transparency<0.3f){
+				transparency=0f;
+				transDirection=!transDirection;
+			}
+
+		} else {
+			transparency+=.005f;
+			if(transparency>.7f){
+				transparency=1f;
+				transDirection=!transDirection;
+			}
+		}
+		Color c = renderer.material.color;
+		c.a = transparency;
+		renderer.material.color = c;
+	}
+}
