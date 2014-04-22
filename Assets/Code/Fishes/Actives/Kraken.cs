7e8aeb1b3699bd0813f7734ffde473f09278d934
Uziel Silva <uziel.silva.espino@gmail.com>
2014-04-21 20:25:04 -0500

    Adding LoadTowersGUI.cs

--- Assets/Code/Fishes/Actives/Kraken.cs
+++ Assets/Code/Fishes/Actives/Kraken.cs
@@ -5,7 +5,7 @@ namespace RoundedDefence.Components.Fishes.Actives
     class Kraken : ActiveFish
     {
         public static Int32 radius = 10;
-        public static Int32 requiredFood = 200;
+        public static Int32 requiredFood = 150;
         public static Double timeToAction = 2.5;
         public static Int32 damage = 1000;
         public Kraken()
---
+++ Assets/Code/Levels/LoadTowerGUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections;
+using RoundedDefence.Components.Fishes;
+
+public class LoadTowerGUI : MonoBehaviour {
+//	public string LevelName;
+	// Use this for initialization
+	void Start () {
+		string passives = "RoundedDefence.Components.Fishes.Passives";
+		string actives = "RoundedDefence.Components.Fishes.Actives";
+
+		var q = from t in Assembly.GetExecutingAssembly().GetTypes()
+			where t.IsClass && !t.IsAbstract && (t.Namespace == passives || t.Namespace == actives)
+				select t;
+		foreach (Type t in q){
+			string s = t.Name;
+			IFish fish = (IFish)Activator.CreateInstance(t);
+			Debug.Log ("The value of property 'RequiredFood' of the class " + s + " is " + fish.RequiredFood);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
