333f54cfd4c8414c37c34c2984c4a1fcfb35a1de
Uziel Silva <uziel.silva.espino@gmail.com>
2014-04-23 23:20:26 -0500

    Adding dinamic property for images.

--- Assets/LoadLevelSelectGUI.cs
+++ Assets/LoadLevelSelectGUI.cs
@@ -5,12 +5,8 @@ using System.Xml.Linq;
 using RoundedDefence;
 
 public class LoadLevelSelectGUI : MonoBehaviour {
-	public Sprite lockedLevelNormal;
-	public Sprite unlockedLevelNormal;
 	public float radiusColliderNormal;
 	public Vector3 scaleLevelNormal;
-	public Sprite lockedLevelSpecial;
-	public Sprite unlockedLevelSpecial;
 	public float radiusColliderSpecial;
 	public Vector3 scaleLevelSpecial;
 	// Use this for initialization
@@ -19,26 +15,36 @@ public class LoadLevelSelectGUI : MonoBehaviour {
 		foreach(XElement level in Lib.data.Element(XName.Get("levels")).Elements(XName.Get("level")))
 		{
 			Debug.Log (level);
-			String id = level.Attribute(XName.Get("id")).Value;
+            String classlevel = level.Attribute(XName.Get("class")).Value;
+            Int16 world = Int16.Parse(level.Attribute(XName.Get("world")).Value);
+            Int16 levelnum = Int16.Parse(level.Attribute(XName.Get("levelnum")).Value);
+            String unlockedSprite = level.Attribute(XName.Get("unlocked-sprite")).Value;
+            String lockedSprite = level.Attribute(XName.Get("locked-sprite")).Value;
+            String id;
+            if (classlevel == "special")
+                id = String.Format("S.{0}.{1}", world, levelnum);
+            else
+                id = String.Format("N.{0}.{1}", world, levelnum);
 			GameObject gameObject = new GameObject("Level"+id);
 			gameObject.AddComponent<SpriteRenderer>();
 			CircleCollider2D col2d=gameObject.AddComponent<CircleCollider2D>();
 			LvlSelect behavior = gameObject.AddComponent<LvlSelect>();
-			behavior.LevelId=id;
-			if(id.Substring(0,1) == "S"){
+            behavior.LevelId = id;
+            behavior.Image1 = Resources.Load("Sprites/Islands/" + lockedSprite) as Sprite;
+            var q = Resources.Load("Sprites/Islands/" + lockedSprite) as Texture2D;
+            behavior.Image2 = Resources.Load("Sprites/Islands/" + unlockedSprite) as Sprite;
+            Debug.Log(behavior.Image1);
+            Debug.Log(behavior.Image2);
+			if(classlevel == "special"){
 				col2d.radius = radiusColliderSpecial;
 				behavior.radius = 0.5f;
-				behavior.speed = 0.01f;
-				behavior.Image1 = unlockedLevelSpecial;
-				behavior.Image2 = lockedLevelSpecial;
-				behavior.centroid = "LevelN"+id.Substring(1);
+                behavior.speed = 0.01f;
+				behavior.centroid = "Level"+id;
 				gameObject.transform.localScale = scaleLevelSpecial;
 			}else{
 				col2d.radius = radiusColliderNormal;	
 				behavior.radius = i;
-				behavior.speed = 0.01f/i;			
-				behavior.Image1 = unlockedLevelNormal;
-				behavior.Image2 = lockedLevelNormal;
+				behavior.speed = 0.01f/i;
 				behavior.centroid="sun";
 				gameObject.transform.localScale = scaleLevelNormal;
 			}
