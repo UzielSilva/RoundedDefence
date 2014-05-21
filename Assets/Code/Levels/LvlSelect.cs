c8b009f77fedf448f7998f4e1d80600dacd32da7
Uziel Silva <uziel.silva.espino@gmail.com>
2014-05-21 05:33:41 -0500

    Reverting unselect option.

--- Assets/Code/Levels/LvlSelect.cs
+++ Assets/Code/Levels/LvlSelect.cs
@@ -12,7 +12,6 @@ public class LvlSelect : MonoBehaviour {
 	public Int16 levelnum;
 	public Int16 stars;
 	private bool enabledd=false;
-
 	public String Image1;
 	public String Image2;
 	public float radius;
@@ -23,8 +22,6 @@ public class LvlSelect : MonoBehaviour {
     GameObject[] starsArr;
 	GameObject sun;
 	Vector3 center;
-	bool mouseover;
-	bool selected;
 	public XElement level;
 	// Use this for initialization
 	void Start () {
@@ -76,18 +73,10 @@ public class LvlSelect : MonoBehaviour {
                 setStars();
 
 		}
-		Vector3 mouse = Camera.main.ScreenToViewportPoint (Input.mousePosition);
-		if (Input.GetMouseButtonDown (0)&& !mouseover && selected  && mouse.y>0.19f) {
-			selected=false;
-			IslandSelected.centroid="";
-		}
-		mouseover = false;
 	}
 	void OnMouseOver()
 	{
-		mouseover = true;
-		if (Input.GetMouseButtonUp (0)) {
-			selected=true;
+		if (Input.GetMouseButtonDown (0)) {
             Lib.setCurrentLevel(classlevel, world, levelnum);
 			IslandSelected.centroid=gameObject.name;
 			GameObject gm=GameObject.Find("_GM");
@@ -95,7 +84,6 @@ public class LvlSelect : MonoBehaviour {
 			if(Lib.sound)
 				gm.audio.Play ();
 		}
-
 	}
 	void setTexture(){
 		SpriteRenderer sprRenderer= (SpriteRenderer)renderer;
