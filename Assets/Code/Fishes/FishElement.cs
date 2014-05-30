5de7175c68e07c51c8809000a633350d90960209
Uziel Silva <uziel.silva.espino@gmail.com>
2014-05-30 01:52:30 -0500

    First fixes.

--- Assets/Code/Fishes/FishElement.cs
+++ Assets/Code/Fishes/FishElement.cs
@@ -1,25 +1,55 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using System.Reflection;
+using System.Linq;
 using RoundedDefence.Components.Fishes;
+using RoundedDefence.Components.Fishes.Actives;
+using RoundedDefence.Components.Fishes.Linears;
+using RoundedDefence.Components.Fishes.Radials;
+using RoundedDefence.Components.Fishes.Roundeds;
+using RoundedDefence.Components.Fishes.Statics;
+using RoundedDefence.Components.Fishes.Targets;
 using RoundedDefence;
 
 public class FishElement : MonoBehaviour {
     public int id;
     public Vector3 position;
     int step;
-    float radius = 4.1f;
     IFish thisFish;
     ShortPath p;
     Vector3 normal;
     Vector3 init;
     float timer;
+    Vector3 center;
+    delegate void FishBehaviour();
+    event FishBehaviour ExecuteBehaviour;
+
+    float radius;
+    float angle;
+    float speed;
 
     // Use this for initialization
 	void Start () {
         thisFish = (IFish)Activator.CreateInstance(Lib.Fishes[id].GetType());
+        if (typeof(ActiveFish).IsAssignableFrom(Lib.Fishes[id].GetType()))
+            ExecuteBehaviour = activesActions;
+        if (typeof(LinearFish).IsAssignableFrom(Lib.Fishes[id].GetType()))
+        {
+            ExecuteBehaviour = linearsActions;
+            center = Vector3.zero;
+            radius = 1;//Vector3.Distance(transform.position, Vector3.zero);
+            angle = 0;
+        }
+        if (typeof(RadialFish).IsAssignableFrom(Lib.Fishes[id].GetType()))
+            ExecuteBehaviour = radialsActions;
+        if (typeof(RoundedFish).IsAssignableFrom(Lib.Fishes[id].GetType()))
+            ExecuteBehaviour = roundedsActions;
+        if (typeof(StaticFish).IsAssignableFrom(Lib.Fishes[id].GetType()))
+            ExecuteBehaviour = staticsActions;
+        if (typeof(TargetFish).IsAssignableFrom(Lib.Fishes[id].GetType()))
+            ExecuteBehaviour = targetsActions;
         transform.position =  position;
-       	//transform.Rotate(Vector3.forward, 90 + Lib.toAngle(angle));
         SpriteRenderer sprRenderer = GetComponent<SpriteRenderer>();
         sprRenderer.sprite = Resources.Load<Sprite>("Sprites/Towers/" + thisFish.Image);
         transform.localScale = (Vector3.one*0.1f);
@@ -34,9 +64,28 @@ public class FishElement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        
+        ExecuteBehaviour();
 	
 	}
-    void goToNextStep()
-    {}
+    void activesActions()
+    { }
+    void linearsActions()
+    {
+        
+    }
+    void radialsActions()
+    { }
+    void roundedsActions()
+    {
+        speed = (float)((RoundedFish)thisFish).Velocity;
+        angle += speed;
+        angle = ((2 * Mathf.PI) + angle) % (Mathf.PI * 2);
+        transform.position = new Vector3(center.x + (radius * Mathf.Cos(angle)), center.y + (radius * Mathf.Sin(angle)), 0f);
+        transform.Rotate(Vector3.forward, speed * 180 / Mathf.PI);
+    
+    }
+    void staticsActions()
+    { }
+    void targetsActions()
+    { }
 }
--- Assets/Code/Fishes/RoundedFish.cs
+++ Assets/Code/Fishes/RoundedFish.cs
@@ -7,7 +7,7 @@ namespace RoundedDefence.Components.Fishes
 
         private Int32[] requiredFood;
         private Int32 requiredStars;
-        private Double[] timeToAction;
+        private Double[] velocity;
         private Int32[] damage;
         private Int32 level;
         private Int32 stamina;
@@ -24,23 +24,23 @@ namespace RoundedDefence.Components.Fishes
         
         public Point Position { get; set; }
         public Int32 RequiredFood { get { return requiredFood[Level-1]; } }
-        public Double TimeToAction { get { return timeToAction[Level-1]; } }
+        public Double Velocity { get { return velocity[Level-1]; } }
 		public Int32 Damage { get { return damage[Level-1]; } }
 		public Int32 Id { get { return id; } }
         public String Image { get { return image; } }
         public String Name { get { return name; } }
 		public float Scale { get { return scale; } }
-		public RoundedFish(Int32[] requiredFood, Int32 requiredStars, Double[] timeToAction, Int32[] damage, Int32[] health , 
+		public RoundedFish(Int32[] requiredFood, Int32 requiredStars, Double[] velocity, Int32[] damage, Int32[] health , 
 		                   string image,string name,Int32 id,float scale)
         {
             if (requiredFood.Length != 4
-                || timeToAction.Length != 4
+                || velocity.Length != 4
                 || damage.Length != 4)
 				throw new Exception("Bad arguments, array's length must be 4.");
 			this.level = 1;
             this.requiredFood = requiredFood;
             this.requiredStars = requiredStars;
-            this.timeToAction = timeToAction;
+            this.velocity = velocity;
             this.damage = damage;
             this.health = health;
 			this.stamina = Health;
