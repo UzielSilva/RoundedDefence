418c4b0bc3320ef0d2d6042e524b2d042fd428a1
german77 <juangerman-13@hotmail.com>
2014-05-30 12:51:07 -0500

    final fix

--- Assets/Code/Background/PointMapListener.cs
+++ Assets/Code/Background/PointMapListener.cs
@@ -5,51 +5,64 @@ using RoundedDefence;
 using RoundedDefence.Components.Fishes;
 
 public class PointMapListener : MonoBehaviour {
-    GameObject shadow;
-    public static int[] costMap = new int[220];
-    bool over;
-
-    public delegate void ClickAction();
-    public delegate void HoverAction(string[] point);
-    public static event ClickAction OnClicked;
-    public static event HoverAction OnHover;
-
+	public bool over;
+	
+	public delegate void ClickAction();
+	public delegate void HoverAction(int point);
+	public static event ClickAction OnClicked;
+	public static event HoverAction OnHover;
+	
 	// Use this for initialization
 	void Start () {
-        over = false;
+		over = false;
 	}
-
-    void OnMouseOver()
-    {
-        if (TowerSelector.idselected != 0)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-
-                GameObject theFish = new GameObject(TowerSelector.idselected + name);
-                theFish.AddComponent<SpriteRenderer>();
-                FishElement theSettings = theFish.AddComponent<FishElement>();
-                theSettings.id = TowerSelector.idselected;
-                theSettings.position = transform.position;
-            }
-        }
-        over = true;
-    }
+	void OnMouseOver()
+	{
+		if (TowerSelector.idselected != 0)
+		{
+			int theInt = Int16.Parse(name.Substring(13));
+			if (OnHover != null)
+				OnHover(theInt);
+			renderer.enabled = true;
+			if (Lib.map[theInt] == 1000)
+				Lib.setSprite(gameObject, "Sprites/Misc/circlered");
+			else
+				Lib.setSprite(gameObject, "Sprites/Misc/circlegreen");
+			if (Input.GetMouseButtonDown(0))
+			{
+				if (Lib.map[theInt] != 1000)
+				{
+					GameObject point2 = GameObject.Find(String.Format("Point{0}", theInt));
+					SpriteRenderer sprRenderer = point2.GetComponent<SpriteRenderer>();
+					sprRenderer.color = Color.red;
+					Lib.map[theInt] = 1000;
+					GameObject theFish = new GameObject(TowerSelector.idselected + name);
+					theFish.AddComponent<SpriteRenderer>();
+					FishElement theSettings = theFish.AddComponent<FishElement>();
+					theSettings.id = TowerSelector.idselected;
+					theSettings.position = transform.position;
+					if(OnClicked != null)
+						OnClicked();
+				}
+			}
+			over = true;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (!over && renderer.enabled == true)
-        {
-            //for (int i = 1; i < 18; i++)
-            //{
-            //    for (int e = 0; e < Lib.getNcircles(i); e++)
-            //    {
-            //        GameObject point2 = GameObject.Find(String.Format("Point{0},{1}", i, e));
-            //        point2.renderer.enabled = false;
-            //    }
-            //}
-            //renderer.enabled = false;
-        }
-        over = false;
+		if (!over && renderer.enabled == true)
+		{
+			for (int i = 13; i < 220; i++)
+			{
+				SpriteRenderer thePoint2 = GameObject.Find(String.Format("Point{0}", i)).GetComponent<SpriteRenderer>();
+				if (Lib.map[i] == 1000)
+					thePoint2.color = Color.red;
+				else
+					thePoint2.color = Color.gray;
+			}
+			renderer.enabled = false;
+		}
+		over = false;
 	}
 }
